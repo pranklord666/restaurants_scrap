03bb5660b73a6135cdac5dad37435d5af2658170
pranklord666 <retropunk.xz@proton.me>
2025-02-25 19:44:32 +0100

    Update run.py

--- app/run.py
+++ app/run.py
@@ -1,6 +1,6 @@
-from app import create_app
+from .main import create_app
 
 app = create_app()
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=10000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=10000)
