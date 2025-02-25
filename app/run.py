73a2e253c57523084b2485d139ac36abaa2b96bb
pranklord666 <retropunk.xz@proton.me>
2025-02-25 23:57:09 +0100

    Update run.py

--- app/run.py
+++ app/run.py
@@ -2,5 +2,9 @@ from app.main import create_app
 
 app = create_app()
 
+# Manually run startup tasks once before starting the server.
+if hasattr(app, "startup_tasks"):
+    app.startup_tasks()
+
 if __name__ == "__main__":
     app.run()
