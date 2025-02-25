acab3037597e45e5214be873f4beefe1e116c37b
pranklord666 <retropunk.xz@proton.me>
2025-02-25 19:15:37 +0100

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,22 +1,26 @@
-from flask import Blueprint, render_template, request, redirect, url_for
-from .scraper import process_feeds
+from flask import Blueprint, render_template, jsonify, request
 from .database import get_articles, update_article_keyword, delete_old_articles
 
 main = Blueprint('main', __name__)
 
-@main.route('/')
+@main.route("/")
 def index():
     articles = get_articles()
-    return render_template('index.html', articles=articles)
+    return render_template("index.html", articles=articles)
 
-@main.route('/update_keyword', methods=['POST'])
-def update_keyword():
-    article_id = request.form['article_id']
-    keyword = request.form['keyword']
-    update_article_keyword(article_id, keyword)
-    return redirect(url_for('main.index'))
+@main.route("/articles")
+def fetch_articles():
+    articles = get_articles()
+    return jsonify(articles)
 
-@main.route('/delete_old_articles')
+@main.route("/update-selection", methods=["POST"])
+def update_selection():
+    data = request.json
+    for article_id, keyword in data.items():
+        update_article_keyword(article_id, keyword)
+    return jsonify({"message": "Selection updated!"})
+
+@main.route("/delete-old", methods=["POST"])
 def delete_old():
     delete_old_articles()
-    return redirect(url_for('main.index'))
+    return jsonify({"message": "Old articles deleted!"})
