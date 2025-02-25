661035987c280f00b189466f2e674f5177d689f9
pranklord666 <retropunk.xz@proton.me>
2025-02-26 00:19:38 +0100

    Create models.py

---
+++ app/models.py
@@ -0,0 +1,15 @@
+from . import db
+
+class Article(db.Model):
+    __tablename__ = "rss_articles"
+    id = db.Column(db.Integer, primary_key=True)
+    date = db.Column(db.String(50))
+    title = db.Column(db.Text, nullable=False)
+    raw_content = db.Column(db.Text)
+    summary = db.Column(db.Text)
+    keyword = db.Column(db.Text)
+    link = db.Column(db.Text, unique=True)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+
+    def __repr__(self):
+        return f"<Article {self.title}>"
