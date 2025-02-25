ee66d65a68c0d976ea56b1eac18bb8be4a1600b8
pranklord666 <retropunk.xz@proton.me>
2025-02-25 19:27:05 +0100

    Update database.py

--- app/database.py
+++ app/database.py
@@ -1,30 +1,32 @@
 import psycopg2
 import os
 
-DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://pranklord:qO1pVU1xCxvV4XKMT2jZgAh81KkLp4m5@dpg-cuuvnhdumphs73f3k6ug-a/restaurants_m0cf")
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 def get_db_connection():
-    conn = psycopg2.connect(DATABASE_URL)
-    return conn
+    if not DATABASE_URL:
+        raise ValueError("DATABASE_URL environment variable not set")
+    
+    return psycopg2.connect(DATABASE_URL)
 
 def get_articles():
     conn = get_db_connection()
     cursor = conn.cursor()
-    cursor.execute("SELECT id, title FROM rss_articles")
+    cursor.execute("SELECT id, title, summary, keyword FROM articles")
     articles = cursor.fetchall()
     conn.close()
-    return [{"id": row[0], "title": row[1]} for row in articles]
+    return [{"id": row[0], "title": row[1], "summary": row[2], "keyword": row[3]} for row in articles]
 
 def update_article_keyword(article_id, keyword):
     conn = get_db_connection()
     cursor = conn.cursor()
-    cursor.execute("UPDATE rss_articles SET keyword = %s WHERE id = %s", (keyword, article_id))
+    cursor.execute("UPDATE articles SET keyword = %s WHERE id = %s", (keyword, article_id))
     conn.commit()
     conn.close()
 
 def delete_old_articles():
     conn = get_db_connection()
     cursor = conn.cursor()
-    cursor.execute("DELETE FROM rss_articles WHERE created_at < NOW() - INTERVAL '1 day'")
+    cursor.execute("DELETE FROM articles WHERE keyword IS NOT NULL")
     conn.commit()
     conn.close()
