aa22777d52f7eb337d83722bff13788e4944fbdb
pranklord666 <retropunk.xz@proton.me>
2025-02-26 01:09:17 +0100

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,11 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
+from flask_migrate import Migrate
 import os
 
 db = SQLAlchemy()
+migrate = Migrate()
 
 def create_app():
     app = Flask(__name__, static_folder='docs', static_url_path='/docs')
@@ -12,8 +14,9 @@ def create_app():
     app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     
-    # Initialize database
+    # Initialize database and migrate
     db.init_app(app)
+    migrate.init_app(app, db)
 
     # Enable CORS for all routes, allowing requests from GitHub Pages
     CORS(app, resources={r"/api/*": {"origins": "https://pranklord666.github.io"}})
